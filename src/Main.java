cc1872b066294cdd7650b36988246d3d80c4d6fa
AvilaGabriel55 <drakobreaker555@gmail.com>
2026-06-24 09:33:05 -0500

    feat: version 2 valida notas entre 0 y 20

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         int nota1 = 15;
-        int nota2 = 25; // Esto es un error lógico, una nota no debería ser 25
-        int promedio = (nota1 + nota2) / 2;
-        System.out.println("El promedio es: " + promedio);
+        int nota2 = 25;
+        
+        if (nota1 < 0 || nota1 > 20 || nota2 < 0 || nota2 > 20) {
+            System.out.println("Error: Las notas deben estar entre 0 y 20");
+        } else {
+            int promedio = (nota1 + nota2) / 2;
+            System.out.println("El promedio es: " + promedio);
+        }
     }
 }
