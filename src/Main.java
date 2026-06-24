2e9ffe934ff81fc79af83d8612f6b285436728fa
AvilaGabriel55 <drakobreaker555@gmail.com>
2026-06-24 09:59:44 -0500

    feat: hace que el programa pida las notas por consola

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        double nota1 = 15.5;
-        double nota2 = 18.0;
+        Scanner teclado = new Scanner(System.in);
+        
+        System.out.print("Ingresa la primera nota: ");
+        double nota1 = teclado.nextDouble();
+        
+        System.out.print("Ingresa la segunda nota: ");
+        double nota2 = teclado.nextDouble();
         
         if (nota1 < 0 || nota1 > 20 || nota2 < 0 || nota2 > 20) {
             System.out.println("Error: Las notas deben estar entre 0 y 20");
@@ -9,5 +16,7 @@ public class Main {
             double promedio = (nota1 + nota2) / 2.0;
             System.out.println("El promedio exacto es: " + promedio);
         }
+        
+        teclado.close();
     }
 }
