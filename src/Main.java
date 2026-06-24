d6f8c86796fc2dfeca5162070a13ea48d7d4e8bb
AvilaGabriel55 <drakobreaker555@gmail.com>
2026-06-24 09:31:06 -0500

    feat: version 1 calcula promedio sin validacion

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int nota1 = 15;
+        int nota2 = 25; // Esto es un error lógico, una nota no debería ser 25
+        int promedio = (nota1 + nota2) / 2;
+        System.out.println("El promedio es: " + promedio);
+    }
+}
