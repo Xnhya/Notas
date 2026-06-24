568413d66a0c7966fed9845c782761f27f792a6b
AvilaGabriel55 <drakobreaker555@gmail.com>
2026-06-24 09:34:04 -0500

    feat: version 3 calcula promedio decimal con double

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        int nota1 = 15;
-        int nota2 = 25;
+        double nota1 = 15.5;
+        double nota2 = 18.0;
         
         if (nota1 < 0 || nota1 > 20 || nota2 < 0 || nota2 > 20) {
             System.out.println("Error: Las notas deben estar entre 0 y 20");
         } else {
-            int promedio = (nota1 + nota2) / 2;
-            System.out.println("El promedio es: " + promedio);
+            double promedio = (nota1 + nota2) / 2.0;
+            System.out.println("El promedio exacto es: " + promedio);
         }
     }
 }
